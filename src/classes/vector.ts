29d4c61bf4f0d9306eaa7f867923301b048a2a91
Alfonso Valencia <alfonsitovale@hotmail.com>
2019-04-01 15:22:56 -0600

    Vector utility
    Added a complete vector utility for mathematical calculations.

--- src/classes/vector.ts
+++ src/classes/vector.ts
@@ -1,25 +1,67 @@
 export default class Vector {
 
-  public x :number;
-  public y :number;
-  public z :number;
+  public x: number;
+  public y: number;
+  public z: number;
 
-  constructor (x :number, y :number, z :number = null) {
+  constructor (x: number, y: number, z: number = null) {
     this.x = x;
     this.y = y;
     this.z = z || 0;
   }
 
-  public multiplyNumber(val :number) :void {
-    this.x *= val;
-    this.y *= val;
-    this.z *= val;
+  /* Getters */
+  public get magnitude(): number {
+    return Math.sqrt(this.squareMagnitude());
   }
 
-  public multiplyVector(vector :Vector) :void {
-    this.x *= vector.x;
-    this.y *= vector.y;
-    this. z*= vector.z;
+  public squareMagnitude(): number {
+    return (this.x * this.x + this.y * this.y + this.z * this.z);
   }
 
+  /* Static methods */
+  public static distance(vA: Vector, vB: Vector) {
+    return Math.pow(Math.pow(vB.x - vA.x, 2) + Math.pow(vB.y - vA.y, 2) + Math.pow(vB.y - vA.y, 2), 1/2);
+  }
+
+  /* Instance methods */
+  public add(vector: Vector): void {
+    this.x += vector.x;
+    this.y += vector.y;
+    this.z += vector.z;
+  }
+
+  public copy(): Vector {
+    return new Vector(this.x, this.y, );
+  }
+
+  public distance(vB: Vector): number {
+    return Math.pow(Math.pow(vB.x - this.x, 2) + Math.pow(vB.y - this.y, 2) + Math.pow(vB.y - this.y, 2), 1/2);
+  }
+
+  public divide(vector: Vector): void {
+    this.x /= vector.x;
+    this.y /= vector.y;
+    this.z /= vector.z;
+  }
+
+  public multiply(number: number): void;
+  public multiply(vector: Vector): void;
+  public multiply(val: any): void {
+    if (val instanceof Vector) {
+      this.x *= val.x;
+      this.y *= val.y;
+      this.z *= val.z;
+    } else if (typeof(val) === 'number') {
+      this.x *= val;
+      this.y *= val;
+      this.z *= val;
+    }
+  }
+
+  public substract(vector: Vector): void {
+    this.x += vector.x;
+    this.y += vector.y;
+    this.z += vector.z;
+  }
 }
